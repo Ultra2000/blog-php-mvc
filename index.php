09ef913c00690d7f706ddd6090d11dcd044cf710
Ultra2000 <frejusbouraima79@gmail.com>
2023-08-18 22:20:05 +0100

    Troisieme commit mvc

--- index.php
+++ index.php
@@ -1,8 +1,34 @@
 <?php
+// index.php
 
-require('src/model.php');
+require_once('src/controllers/add_comment.php');
+require_once('src/controllers/homepage.php');
+require_once('src/controllers/post.php');
 
-$posts = getPosts();
+try {
+	if (isset($_GET['action']) && $_GET['action'] !== '') {
+    	if ($_GET['action'] === 'post') {
+        	if (isset($_GET['id']) && $_GET['id'] > 0) {
+            	$identifier = $_GET['id'];
 
-require('templates/homepage.php');
-?>
+            	post($identifier);
+        	} else {
+            	throw new Exception('Aucun identifiant de billet envoyé');
+        	}
+    	} elseif ($_GET['action'] === 'addComment') {
+        	if (isset($_GET['id']) && $_GET['id'] > 0) {
+            	$identifier = $_GET['id'];
+
+            	addComment($identifier, $_POST);
+        	} else {
+            	throw new Exception('Aucun identifiant de billet envoyé');
+        	}
+    	} else {
+        	throw new Exception("La page que vous recherchez n'existe pas.");
+    	}
+	} else {
+    	homepage();
+	}
+} catch (Exception $e) { // S'il y a eu une erreur, alors...
+	echo 'Erreur : '.$e->getMessage();
+}
--- src/model.php
+++ src/model.php
@@ -2,11 +2,7 @@
 
 function getPosts() {
 	// We connect to the database.
-	try {
-    	$database = new PDO('mysql:host=localhost;dbname=blog;charset=utf8', 'blog-mvc', '');
-	} catch(Exception $e) {
-    	die('Erreur : '.$e->getMessage());
-	}
+	$database = dbConnect();
 
 	// We retrieve the 5 last blog posts.
 	$statement = $database->query(
@@ -18,11 +14,61 @@ function getPosts() {
         	'title' => $row['titre'],
         	'french_creation_date' => $row['date_creation_fr'],
         	'content' => $row['contenu'],
+			'identifier' => $row['id'],
     	];
 
     	$posts[] = $post;
 	}
 
 	return $posts;
+}
+
+function getPost($identifier) {
+	$database = dbConnect();
+
+	$statement = $database->prepare("SELECT id, title, content, DATE_FORMAT(creation_date, '%d/%m/%Y à %Hh%min%ss') AS french_creation_date FROM posts WHERE id = ?");
+	$statement->execute([$identifier]);
+
+	$row = $statement->fetch();
+	$post = [
+		'title' => $row['title'],
+		'french_creation_date' => $row['french_creation_date'],
+		'content' => $row['content'],
+		'identifier' => $row['id'],
+	];
+
+	return $post;
+
+}
+
+function getComments($identifier) {
+	$database = dbConnect();
+	
+	$statement = $database->prepare("SELECT id, author, comment, DATE_FORMAT(comment_date, '%d/%m/%Y à %Hh%imin%ss') AS french_creation_date FROM comments WHERE post_id = ? ORDER BY comment_date DESC");
+	$statement->execute($identifier);
+
+	$comments = [];
+
+	while (($row = $statement->fetch())) {
+		$comment = [
+			'author' => $row['author'],
+			'french_creation_date' => $row['french_creation_date'],
+			'comment' => $row['comments'],
+		];
+
+		$comments[] = $comment;
+	}
+
+	return $comments;
+}
+
+
+function dbConnect() {
+	try{
+		$database = new PDO('mysql:host=localhost;dbname=blog;charset=utf8', 'blog-open', '');		
+		return $database;
+	} catch(Exception $e) {
+		die('Erreur: '.$e->getMessage());
+	}
 }
 
